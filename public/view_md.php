57a7862f83696d8be692fe8a053ae0c3f83a3a4b
Sebastian PW <sebastian.wolbring@gmail.com>
2025-10-25 17:17:08 +0200

    removed warnings

--- public/view_md.php
+++ public/view_md.php
@@ -12,7 +12,8 @@
 require_once __DIR__ . '/bootstrap.php';
 require __DIR__ . '/env_locals.php';
 
-use Parsedown;
+error_reporting(0);
+ini_set('display_errors', '0');
 
 $spw = \App\Core\SpwBase::getInstance();
 
