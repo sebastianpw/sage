7451e22d0f01143706ff1d505a11741ff22fad54
Sebastian PW <sebastian.wolbring@gmail.com>
2025-11-02 10:50:03 +0100

    removed

--- public/create_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "bootstrap.php";
-
-$password = password_hash('password', PASSWORD_DEFAULT);
-$stmt = $pdo->prepare("INSERT INTO `user` (`username`, `password`, `name`, `role`) VALUES (?, ?, ?, ?)");
-$result = $stmt->execute(['admin', $password, 'Peter Sebring', 'admin']);
-var_dump($result);
-
